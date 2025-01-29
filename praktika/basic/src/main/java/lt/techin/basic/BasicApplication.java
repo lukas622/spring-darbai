79eb23744cf356db54792231187bd49a10ed8898
D2JP <d2jp@TECHIN.INTERNAL>
2025-01-29 11:04:59 +0200

    Simple Controller

---
+++ praktika/basic/src/main/java/lt/techin/basic/BasicApplication.java
@@ -0,0 +1,13 @@
+package lt.techin.basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class BasicApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BasicApplication.class, args);
+  }
+}
---
+++ praktika/basic/src/test/java/lt/techin/basic/BasicApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.techin.basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
